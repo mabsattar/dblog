0b3b5e81a44e6a558602d6c7af83ef6d00a953d0
AbSattar <mdabdullsattar@gmail.com>
2023-12-03 03:38:33 +0530

    setting up my dapp

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Navbar from './Components/Navbar'
+import "@rainbow-me/rainbowkit/styles.css" ;
+import "../app/globals.css";
+import Navbar from './Components/Navbar';
+import { configureChains } from "wagmi";
+
+//Chains we are going to support
+//"Connectors" for these chains
+//wagmiclient
+
+const {chains, provider} = configureChains
 
 export default function Home() {
   return (
